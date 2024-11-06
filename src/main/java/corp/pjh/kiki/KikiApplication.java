ad59260d69db895bbe1476847e07729ae0a8df72
박진혁 <park@bagjinhyeog-ui-MacBookAir.local>
2024-11-06 17:55:50 +0900

    add: 프로젝트 기본 설정
    각종 라이브러리, 도커 셋업 등

---
+++ src/main/java/corp/pjh/kiki/KikiApplication.java
@@ -0,0 +1,13 @@
+package corp.pjh.kiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/corp/pjh/kiki/KikiApplicationTests.java
@@ -0,0 +1,13 @@
+package corp.pjh.kiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
